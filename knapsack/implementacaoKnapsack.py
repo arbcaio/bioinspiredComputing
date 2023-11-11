9b7cd846f57144a49ff069f3ce0d969a2d98950a
arbcaio <chab09@gmail.com>
2023-11-11 11:31:30 -0300

    Import path bug fix

--- knapsack/implementacaoKnapsack.py
+++ knapsack/implementacaoKnapsack.py
@@ -1,48 +1,26 @@
-import random
+import sys
+sys.path.insert(0, 'C:\\Users\\caioc\\OneDrive\\Documentos\\UFPE\\2023.2\\Bioinspirada\\bioinspiredComputing')
 import utils
 
 # Definindo parâmetros do problema da mochila
-quant_items = 100
+quant_items = 10
 items = []
 max_peso = 50 
 max_valor = 1000
 max_peso_mochila = 100
 quant_individuos = 25
-taxa_mutacao = 0.25
+taxa_mutacao = 0.1
 quant_geracoes = 100
 quant_iteracoes = 100
 
-# Algoritmo genético para resolver o problema da mochila
-def algoritmo_genetico():
-    # Inicialização da população
-    populacao = [utils.gerar_individuo() for _ in range(quant_individuos)]
+items = utils.gerar_items(quant_items, max_peso, max_valor)
+utils.limpar_tela()
+utils.display_items(items, sorted_list=0)
 
-    # Loop de gerações
-    for geracao in range(quant_geracoes):
-        # Avaliação da aptidão da população
-        aptidoes = [utils.calcular_valor(solucao) for solucao in populacao]
+# populacao = utils.gerar_populacao_inicial(quant_items, quant_individuos)
+# print('individuos gerados:', len(populacao))
 
-        # Seleção dos pais com base na aptidão
-        pais = random.choices(populacao, weights=aptidoes, k=quant_individuos)
-
-        # Criação da nova geração usando crossover e mutação
-        nova_populacao = []
-        for i in range(0, quant_individuos, 2):
-            pai1, pai2 = pais[i], pais[i + 1]
-            filho = utils.crossover(pai1, pai2)
-            filho = utils.mutacao(filho)
-            nova_populacao.extend([pai1, pai2, filho])
-
-        populacao = nova_populacao
-
-    # Encontrar a melhor solução
-    melhor_individuo = max(populacao, key=utils.calcular_valor)
-    melhor_valor = utils.calcular_valor(melhor_individuo)
-    melhor_peso = utils.calcular_peso(melhor_individuo)
-
-    print(f'Melhor solução: {melhor_individuo}')
-    print(f'Valor: {melhor_valor}')
-    print(f'Peso: {melhor_peso}')
-
-# Executar o algoritmo genético
-algoritmo_genetico()
+individuo = utils.gerar_individuo(quant_items, max_peso_mochila, items)
+print('individuo:', individuo)
+peso = utils.calcular_peso(individuo, items)
+print('peso:', peso)
